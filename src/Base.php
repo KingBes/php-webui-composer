c07cfabe9f13fb9c2eeaae724d794f446373bae9
快乐两小时 <1164601554@qq.com>
2024-08-09 23:07:51 +0800

    20240809

---
+++ src/Base.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kingbes;
+
+class Base
+{
+    public \FFI $ffi;
+
+    /**
+     * 构造函数 function
+     *
+     * @param string|null $libraryFile 库文件路径（库文件在别处的时候使用）
+     */
+    public function __construct(
+        protected ?string $libraryFile = null
+    ) {
+        $header = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "webui_php.h");
+        $this->ffi = \FFI::cdef($header, $this->library());
+    }
+
+    /**
+     * 库文件 function
+     *
+     * @return string
+     */
+    protected function library(): string
+    {
+        if ($this->libraryFile !== null) {
+            return $this->libraryFile;
+        }
+
+        $this->libraryFile = match (PHP_OS_FAMILY) {
+            'Linux'   => __DIR__ . DIRECTORY_SEPARATOR . "os" . DIRECTORY_SEPARATOR  . "webui-2.so",
+            'Darwin'  => __DIR__ . DIRECTORY_SEPARATOR . "os" . DIRECTORY_SEPARATOR  . "webui-2.dylib",
+            'Windows' => __DIR__ . DIRECTORY_SEPARATOR . "os" . DIRECTORY_SEPARATOR  . "webui-2.dll",
+            default   => throw new \Exception("Os is not supported, Only Linux, MacOs and windows are only supported by default!")
+        };
+
+        return $this->libraryFile;
+    }
+}
--- src/Webui.php
+++ src/Webui.php
@@ -4,41 +4,98 @@ declare(strict_types=1);
 
 namespace Kingbes;
 
-class Webui
+use Closure;
+
+class Webui extends Base
 {
-    protected \FFI $ffi;
-
-    protected mixed $window;
-
-    public function __construct()
+    /**
+     * 创建一个新的web窗口对象。 function
+     *
+     * @return int
+     */
+    public function newWindow(): int
     {
-        if (PHP_OS_FAMILY != "Windows") {
-            throw new \Exception("The OS must be windows!");
-        }
-        $header = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "webui_php.h");
-        $this->ffi = \FFI::cdef($header, __DIR__ . DIRECTORY_SEPARATOR . "os" . DIRECTORY_SEPARATOR  . "webui-2.dll");
+        return $this->ffi->webui_new_window();
     }
 
-    public function newWindow(): self
+    /**
+     * 使用指定的窗口号创建新的web窗口对象。
+     *
+     * @param integer $windows web窗口对象
+     * @return integer
+     */
+    public function newWindowId(int $windows): int
     {
-        $this->window = $this->ffi->webui_new_window();
-        return $this;
+        return $this->ffi->webui_new_window_id($windows);
     }
 
-    public function show(string $html): self
+    /**
+     * 获得一个可以使用的免费窗口对象
+     *
+     * @return integer
+     */
+    public function getNewWindowId(): int
     {
-        $this->ffi->webui_show($this->window, $html);
-        return $this;
+        return $this->ffi->webui_get_new_window_id();
     }
 
-    public function wait(): self
+    /**
+     * 用后端函数绑定一个HTML元素和一个JavaScript对象。空元素名称表示所有事件。 function
+     *
+     * @param integer $windows 窗口对象
+     * @param string $element 绑定js函数名
+     * @param Closure $func php函数
+     * @return integer
+     */
+    public function bind(
+        int $windows,
+        string $element,
+        Closure $func
+    ): int {
+
+        $callback = $this->ffi->new('void (*)(webui_event_t *)');
+        // $callback = $callbackPtr->cdata;
+        $callback = function ($event) use ($func) {
+            /* $phpEvent = [
+                'window' => $event->window,
+                'event_type' => $event->event_type,
+                'element' => \FFI::string($event->element),
+                'event_number' => $event->event_number,
+                'bind_id' => $event->bind_id,
+                'client_id' => $event->client_id,
+                'connection_id' => $event->connection_id,
+                'cookies' => \FFI::string($event->cookies)
+            ]; */
+            $func($event);
+        };
+        return $this->ffi->webui_bind($windows, $element, $callback);
+    }
+
+    public function getString(Event $e): string
+    {
+        return $this->ffi->webui_get_string($e);
+    }
+
+    /**
+     * 使用嵌入的HTML或文件显示窗口。如果窗口已经打开，它将被刷新。这将刷新多客户端模式下的所有窗口。
+     *
+     * @param integer $windows web窗口对象
+     * @param string $content html内容
+     * @example 示例 show($myWindow,"<html>...</html>"); | show($myWindow, "index.html"); | show($myWindow, "http://...");
+     * @return boolean
+     */
+    public function show(int $windows, string $content): bool
+    {
+        return $this->ffi->webui_show($windows, $content);
+    }
+
+    /**
+     * 等到所有开着的窗户都关上了。
+     *
+     * @return void
+     */
+    public function wait(): void
     {
         $this->ffi->webui_wait();
-        return $this;
-    }
-
-    public function destroy(): void
-    {
-        $this->ffi->webui_destroy($this->window);
     }
 }
